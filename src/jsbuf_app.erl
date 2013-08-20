083f6f62e04dfc8e057f7feef4bf2c4c9a06f868
Tyler Margison <kolorahl@gmail.com>
2013-08-20 16:39:05 -0700

    Initial rebar commit and generator app creation

---
+++ src/jsbuf_app.erl
@@ -0,0 +1,16 @@
+-module(jsbuf_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    jsbuf_sup:start_link().
+
+stop(_State) ->
+    ok.
